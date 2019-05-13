4eeb0c386925a898adcd62a3b2d6a56acda72e65
ao <2803806216@qq.com>
2019-05-14 07:59:47 +0800

    test

---
+++ src/com/test/web/action/CaptcharResult.java
@@ -0,0 +1,17 @@
+package com.test.web.action;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import org.apache.struts2.dispatcher.StrutsResultSupport;
+
+/**
+ * @Description TODO (用一句话描述该文件做什么)
+ * @Author Zengsl
+ * @Date 2019/5/14 7:51
+ * @Version V1.0
+ */
+public class CaptcharResult extends StrutsResultSupport {
+    @Override
+    protected void doExecute(String s, ActionInvocation actionInvocation) throws Exception {
+
+    }
+}
